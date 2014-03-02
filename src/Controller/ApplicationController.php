cbb018c56b5fd0fcb3e9335ce4898f9ea1ed4c57
FB <fiona@purencool.com.au>
2014-03-02 19:55:57 +1100

    Updates menu

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -7,16 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ApplicationController
 {
-   public function indexAction(Application $app)
+    public function indexAction(Application $app)
     {	
         return $app['twig']->render('index.html.twig');
     }
    
-   public function contactAction(Application $app)
+    public function contactAction(Application $app)
     {	
         return $app['twig']->render('contact.html.twig');
     }
-   public function aboutAction(Application $app)
+	
+    public function aboutAction(Application $app)
     {	
         return $app['twig']->render('about.html.twig');
     }

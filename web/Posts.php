da3f045840045372de4b25c42761ed72c784ee30
Purencool website development <purencool@gmail.com>
2014-03-21 18:01:12 +1100

    This refs #11 and I cleaned code up in the twig files.

--- web/Posts.php
+++ web/Posts.php
@@ -2,9 +2,6 @@
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader as YamlRouting;
 use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Translation\Loader\YamlFileLoader;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
 
@@ -17,12 +14,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../src/Resources/view/',
 ));
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+$app['twig'] = $app->share($app->extend('twig', function($twig) use ($app){
     return $twig;
 }));
 
 //-- Loading yaml url routes file is kept and then loading file 
-$app['routes'] = $app->extend('routes', function (RouteCollection $routes, $app) {
+$app['routes'] = $app->extend('routes', function (RouteCollection $routes) use ($app) {
+
     $loader     = new YamlRouting(new FileLocator(__DIR__ . '/../src/Resources/config'));
     $collection = $loader->load('routes.yml');
     $routes->addCollection($collection);

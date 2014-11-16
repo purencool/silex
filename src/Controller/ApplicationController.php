c4bf3747c86ce000848f0aeef7121d9503195239
purencool <purencool@gmail.com>
2014-11-16 17:54:16 +1100

    Fixes to the merge

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Routes the html requests to the correct twig
- * template files 
+ * template files
  *
  * @package    Trace
- * @category   
+ * @category
  * @author     Purencool Website Development
  * @license    GPL3
  */
@@ -17,12 +17,13 @@ class ApplicationController {
 
     /**
      *  Build login form
-     *  
+     *
      *  @param array $app application container
      *  @return array building login form
-     *  
+     *
      */
-    private function loginForm(Application $app) {
+    private function loginForm(Application $app)
+    {
         $data = array();
         $formBuild = $app['form.factory']->createBuilder(new LoginForm(), $data);
         $form = $formBuild->getForm();
@@ -35,52 +36,51 @@ class ApplicationController {
 
     /**
      *  Index action controller
-     *  
+     *
      *  @param array $app application container
      *  @return array for twig templating file
-     *  
+     *
      */
-    public function indexAction(Application $app) {
+    public function indexAction(Application $app)
+    {
         $param = $this->loginForm($app);
         return $app['twig']->render('index.html.twig', $param);
     }
 
     /**
      *  Contact action controller
-     *  
+     *
      *  @param array $app application container
      *  @return array for twig templating file
-     *  
+     *
      */
-    public function contactAction(Application $app) {
+    public function contactAction(Application $app)
+    {
         $param = $this->loginForm($app);
         return $app['twig']->render('contact.html.twig', $param);
     }
 
     /**
      *  About action controller
-     *  
+     *
      *  @param array $app application container
      *  @return array for twig templating file
-     *  
+     *
      */
-    public function aboutAction(Application $app) {
+    public function aboutAction(Application $app)
+    {
         $param = $this->loginForm($app);
         return $app['twig']->render('about.html.twig', $param);
     }
-<<<<<<< HEAD
     /**
      *  User pannel action controller
-     *  
+     *
      *  @param array $app application container
      *  @return array for twig templating file
-     *  
+     *
      */
-    public function userPanelAction(Application $app) {
-=======
     public function userPanelAction(Application $app)
     {
->>>>>>> 9e7360692498ae6e8a11e5ca18bba3b4c2d85bbb
         return $app['twig']->render('user_panel.html.twig');
     }
 }
